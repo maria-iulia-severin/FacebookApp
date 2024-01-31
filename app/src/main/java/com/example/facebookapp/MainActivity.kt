315e167b8dc9c5d69c7c7994e89b70816b845be0
iulia-maria-severin <iulia.severin12@gmail.com>
2024-01-31 13:51:27 +0200

    Navigation Drawer

--- app/src/main/java/com/example/facebookapp/MainActivity.kt
+++ app/src/main/java/com/example/facebookapp/MainActivity.kt
@@ -5,19 +5,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.facebookapp.ui.theme.FacebookAppTheme
+import kotlinx.coroutines.launch
 
 
 sealed class Destination(val route: String) {
@@ -49,15 +52,32 @@ class MainActivity : ComponentActivity() {
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun FBScaffold(navController: NavHostController) {
+
+    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+    val scope = rememberCoroutineScope()
+
     val onDrawerClicked: () -> Unit = {
+        scope.launch { drawerState.apply { if (isClosed) open() else close() } }
     }
-    Scaffold(
-        bottomBar = { FBBottomNavigation(navController, onDrawerClicked) }
-    ) {
-        NavHost(navController = navController, startDestination = Destination.Home.route) {
-            composable(Destination.Home.route) { HomeScreen() }
-            composable(Destination.Notifications.route) { NotificationScreen() }
+
+    ModalNavigationDrawer(
+        drawerState = drawerState,
+        drawerContent = {
+            ModalDrawerSheet {
+                NavigationDrawer()
+            }
+        }) {
+
+        Scaffold(
+            bottomBar = { FBBottomNavigation(navController, onDrawerClicked) }
+        ) {
+            NavHost(navController = navController, startDestination = Destination.Home.route) {
+                composable(Destination.Home.route) { HomeScreen() }
+                composable(Destination.Notifications.route) { NotificationScreen() }
+            }
         }
+
     }
+
 
 }
